a6efc21ee7d6631b24106e558f19f10377b839a1
Gabriel Daiki <daiki.gab@gmail.com>
2025-11-29 15:43:13 -0300

    docs: add auth grant type instructions

--- src/main/java/com/ecommerce/auth_server/dbinit/DatabaseInitializer.java
+++ src/main/java/com/ecommerce/auth_server/dbinit/DatabaseInitializer.java
@@ -50,6 +50,7 @@ public class DatabaseInitializer implements CommandLineRunner {
                         .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                         .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                         .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
                         .redirectUri("http://127.0.0.1:8080/login/oauth2/code/client")
                         .scope(OidcScopes.OPENID)
                         .scope(OidcScopes.PROFILE)
